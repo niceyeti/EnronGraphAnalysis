b3e95e49b7ecbaf5b81a60f2c19ec31d28afc6f7
Jesse Waite <jwaite@eecs.wsu.edu>
2016-04-11 18:17:22 -0700

    checkpoint

--- Spectrum.py
+++ Spectrum.py
@@ -1,15 +1,18 @@
 from __future__ import print_function
 import networkx
 from numpy import linalg as la
-
+from scipy import sparse as sp
 
 print("Building graph...")
 g = networkx.read_graphml("./enronUndirected.graphml")
-print("graph:"+str(g))
+print("graph of "+str(g.number_of_nodes())+" nodes:"+str(g))
 print("Getting laplacian...")
-l = networkx.laplacian_matrix(g)
+l = networkx.normalized_laplacian_matrix(g)
+#print(str(l))
 print("Computing eigenvalues...")
-e,v = la.eig(l)
+#e,v = la.eig(l.A) #explodes
+w,v = sp.linalg.eigs(l.A)
+print("done")
 
 
 
