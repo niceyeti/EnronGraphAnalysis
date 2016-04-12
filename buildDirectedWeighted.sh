d7d2d07e67d3bb3a5a304a09397580ecddfb544a
Jesse Waite <Jesse_Waite@hotmail.com>
2016-04-11 17:24:58 -0700

    checkpoint

--- buildDirectedWeighted.sh
+++ buildDirectedWeighted.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Builds a weighted, directed graph, disallowing reflexive loops (self-emails), filtering any edges between peers sharing fewer than 3 emails
-python BuildGraph.py ./maildir enronGraph.graphml --directed --weighted --disallowReflexive --filterFrequency=3
+python BuildGraph.py ./maildir enronDirected.graphml --directed --weighted --disallowReflexive --filterFrequency=3
