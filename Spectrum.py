6283abba799ab660f790dc83944be6697a24ddd9
Jesse Waite <Jesse_Waite@hotmail.com>
2016-04-11 18:22:59 -0700

    chekcpoint

--- Spectrum.py
+++ Spectrum.py
@@ -11,9 +11,29 @@ l = networkx.normalized_laplacian_matrix(g)
 #print(str(l))
 print("Computing eigenvalues...")
 #e,v = la.eig(l.A) #explodes
-w,v = sp.linalg.eigs(l.A)
+w,v = sp.linalg.eigs(l)
 print("done")
 
 
+"""
 
+Create an G{n,m} random graph and compute the eigenvalues.
+Requires numpy and matplotlib.
+
+import networkx as nx
+import numpy.linalg
+import matplotlib.pyplot as plt
+
+n = 1000 # 1000 nodes
+m = 5000 # 5000 edges
+G = nx.gnm_random_graph(n,m)
+
+L = nx.normalized_laplacian_matrix(G)
+e = numpy.linalg.eigvals(L.A)
+print("Largest eigenvalue:", max(e))
+print("Smallest eigenvalue:", min(e))
+plt.hist(e,bins=100) # histogram with 100 bins
+plt.xlim(0,2)  # eigenvalues between 0 and 2
+plt.show()
+"""
 
