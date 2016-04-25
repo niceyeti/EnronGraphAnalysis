2d169fb5bec296cc10b7344b39c3842c2e09c6a1
Jesse Waite <Jesse_Waite@hotmail.com>
2016-04-24 19:19:45 -0700

    checkpoint

--- runAll.sh
+++ runAll.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
-#sh buildUndirected.sh
-#sh buildDirected.sh
+#Each command in this file must contain a single space at the end of each line, or bash/python will complain about CR's
+sh buildUndirected.sh 
+sh buildDirected.sh 
 python AnalyzeGraph.py enronUndirected.graphml report_undirected 
 python AnalyzeGraph.py enronDirected.graphml report_directed 
 echo analysis completed.
